8bb32ee96031ab69c35ee3b33d97d492fb2ad913
Programador Cego <programadorcego@gmail.com>
2023-03-22 21:05:45 -0300

    Aula 17: Exercício #5

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 /*
-Crie um objeto chamado carrinho_de_compras com as propriedades itens (um array) e precoTotal.
-Preencha o array itens com objetos representando itens no carrinho de compras, com cada objeto contendo as propriedades nome e preco.
-Use um loop forEach para exibir cada nome de item e preço no console.
-Em seguida, use reduce para calcular o preço total de todos os itens no carrinho de compras.
+Crie um array de objetos representando restaurantes, com cada objeto de restaurante contendo as propriedades nome, culinaria e avaliacao.
+Use o método filter para criar um novo array contendo apenas os restaurantes com classificação 4 ou superior.
+Em seguida, use map para criar outro array contendo apenas os nomes desses restaurantes altamente cotados.
 */
 
-const carrinho_de_compras = {
-  itens: [
-    { nome: 'Camiseta Branca', preco: 39.90 },
-    { nome: 'Calça Jeans', preco: 129.90 },
-    { nome: 'Tênis Esportivo', preco: 259.90 },
-    { nome: 'Jaqueta de Couro', preco: 599.90 },
-    { nome: 'Óculos de Sol', preco: 189.90 },
-    { nome: 'Relógio de Pulso', preco: 799.90 },
-    { nome: 'Bolsa de Couro', preco: 349.90 },
-    { nome: 'Fones de Ouvido Sem Fio', preco: 199.90 },
-    { nome: 'Smartwatch', preco: 899.90 },
-    { nome: 'Notebook', preco: 4599.90 },
-  ],
-  precoTotal: 0,
-};
+const restaurantes = [
+  { nome: 'Restaurante A', culinaria: 'Italiana', avaliacao: 4.5 },
+  { nome: 'Restaurante B', culinaria: 'Japonesa', avaliacao: 3.2 },
+  { nome: 'Restaurante C', culinaria: 'Brasileira', avaliacao: 2.8 },
+  { nome: 'Restaurante D', culinaria: 'Mexicana', avaliacao: 4.9 },
+  { nome: 'Restaurante E', culinaria: 'Indiana', avaliacao: 3.5 },
+  { nome: 'Restaurante F', culinaria: 'Francesa', avaliacao: 2.1 },
+  { nome: 'Restaurante G', culinaria: 'Chinesa', avaliacao: 4.2 },
+  { nome: 'Restaurante H', culinaria: 'Coreana', avaliacao: 3.9 },
+  { nome: 'Restaurante I', culinaria: 'Portuguesa', avaliacao: 4.7 },
+  { nome: 'Restaurante J', culinaria: 'Mediterrânea', avaliacao: 3.0 },
+];
 
-carrinho_de_compras.itens.forEach(item => console.log(`Nome: ${item.nome} Preço: ${item.preco}`));
+const restaurantes_avaliados = restaurantes.filter(r => r.avaliacao >= 4);
+//console.log(restaurantes_avaliados);
 
-carrinho_de_compras.precoTotal = carrinho_de_compras.itens.reduce((total, item) => total + item.preco, carrinho_de_compras.precoTotal);
-console.log(carrinho_de_compras.precoTotal);
+const restaurantes_nomes = restaurantes_avaliados.map(r => r.nome);
+console.log(restaurantes_nomes);
