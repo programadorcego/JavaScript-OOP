21448988143703a0debcb3df6b8ac1b18e3a3fa5
Programador Cego <programadorcego@gmail.com>
2023-03-22 21:01:49 -0300

    Aula 15: Exercício #3

--- main.js
+++ main.js
@@ -1,16 +1,64 @@
 /*
-Escreva uma função que receba um array de números como parâmetro e retorne um objeto com a soma e a média de todos os números.
+Crie um array de objetos representando livros, com cada objeto contendo as propriedades `título`, `autor` e `ano`.
+Use o método `map` para criar um novo array contendo apenas os títulos dos livros.
+Em seguida, use o método `filter` para criar outro array contendo apenas os livros publicados após 1990.
 */
 
+const livros = [
+  {
+    titulo: 'Harry Potter e a Pedra Filosofal',
+    autor: 'J.K. Rowling',
+    ano: 1997
+  },
+  {
+    titulo: 'O Senhor dos Anéis: A Sociedade do Anel',
+    autor: 'J.R.R. Tolkien',
+    ano: 1954
+  },
+  {
+    titulo: 'A Menina que Roubava Livros',
+    autor: 'Markus Zusak',
+    ano: 2005
+  },
+  {
+    titulo: '1984',
+    autor: 'George Orwell',
+    ano: 1949
+  },
+  {
+    titulo: 'Orgulho e Preconceito',
+    autor: 'Jane Austen',
+    ano: 1813
+  },
+  {
+    titulo: 'Cem Anos de Solidão',
+    autor: 'Gabriel Garcia Marquez',
+    ano: 1967
+  },
+  {
+    titulo: 'A Insustentável Leveza do Ser',
+    autor: 'Milan Kundera',
+    ano: 1984
+  },
+  {
+    titulo: 'A Revolução dos Bichos',
+    autor: 'George Orwell',
+    ano: 1945
+  },
+  {
+    titulo: 'O Grande Gatsby',
+    autor: 'F. Scott Fitzgerald',
+    ano: 1925
+  },
+  {
+    titulo: 'As Aventuras de Huckleberry Finn',
+    autor: 'Mark Twain',
+    ano: 1884
+  }
+];
 
-const numeros = [1, 2, 3, 4, 5, 6];
+const livros_nome = livros.map(l => l.titulo);
+//console.log(livros_nome);
 
-function obterSomaMedia(arr)
-{
-	const soma = arr.reduce((total, n) => total + n);
-	const media = soma / arr.length;
-	
-	return {soma, media};
-}
-
-console.log(obterSomaMedia(numeros));
+const livros_90 = livros.filter(l => l.ano >= 1990);
+console.log(livros_90);
