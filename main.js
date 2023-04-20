97e21c7e75b78e49fada22f658c5b963c453cfd0
Programador Cego <programadorcego@gmail.com>
2023-04-20 07:34:04 -0300

    Aula 23: Encapsulamento

--- main.js
+++ main.js
@@ -1,34 +1,15 @@
-/*
-Herança é uma forma de compartilhar métodos e propriedades entre diferentes objetos.
-No JavaScript, a herança é implementada através do conceito de protótipos.
-Um protótipo é um objeto que contém propriedades e métodos que podem ser acessados por outros objetos.
-Quando um objeto precisa de uma propriedade ou método que não está presente em si mesmo, ele procura no seu protótipo até encontrar o que precisa.
-*/
-
 class Pessoa {
-	constructor(nome, idade){
-		this.nome = nome;
-		this.idade = idade;
+	#nome;
+	
+	constructor(nome){
+		this.#nome = nome;
 	}
 	
-	cumprimentar(){
-		console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
+	cumprimentar()
+	{
+		console.log(`Olá, meu nome é ${this.#nome}`);
 	}
 }
 
-class Estudante extends Pessoa {
-	constructor(nome, idade, curso){
-		super(nome, idade);
-		this.curso = curso;
-	}
-	
-	cumprimentar(){
-		console.log(`Olá, meu nome é ${this.nome} e sou estudante do curso de ${this.curso}.`);
-	}
-}
-
-const pessoa = new Pessoa("Willian", 32);
+const pessoa = new Pessoa("Willian");
 pessoa.cumprimentar();
-
-const estudante = new Estudante(pessoa.nome, pessoa.idade, "Programação");
-estudante.cumprimentar();
