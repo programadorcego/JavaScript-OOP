3d33ce19459f41cedfba6d1ebc5197186fa8c8d0
Programador Cego <programadorcego@gmail.com>
2023-04-20 07:33:16 -0300

    Aula 22: Herança

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
 /*
-Classes são modelos de códigos para criar objetos.
-No JavaScript,criamos uma classe com a palavra reservada class. 
+Herança é uma forma de compartilhar métodos e propriedades entre diferentes objetos.
+No JavaScript, a herança é implementada através do conceito de protótipos.
+Um protótipo é um objeto que contém propriedades e métodos que podem ser acessados por outros objetos.
+Quando um objeto precisa de uma propriedade ou método que não está presente em si mesmo, ele procura no seu protótipo até encontrar o que precisa.
 */
 
 class Pessoa {
-	constructor(nome, sobrenome, idade){
+	constructor(nome, idade){
 		this.nome = nome;
-		this.sobrenome = sobrenome;
 		this.idade = idade;
 	}
 	
 	cumprimentar(){
-		return `Meu nome é ${this.nome} ${this.sobrenome} e tenho ${this.idade} anos`;
+		console.log(`Olá, meu nome é ${this.nome} e tenho ${this.idade} anos.`);
 	}
 }
 
-const p1 = new Pessoa("Willian", "Pereira", 32)
-console.log(p1.cumprimentar());
+class Estudante extends Pessoa {
+	constructor(nome, idade, curso){
+		super(nome, idade);
+		this.curso = curso;
+	}
+	
+	cumprimentar(){
+		console.log(`Olá, meu nome é ${this.nome} e sou estudante do curso de ${this.curso}.`);
+	}
+}
+
+const pessoa = new Pessoa("Willian", 32);
+pessoa.cumprimentar();
+
+const estudante = new Estudante(pessoa.nome, pessoa.idade, "Programação");
+estudante.cumprimentar();
