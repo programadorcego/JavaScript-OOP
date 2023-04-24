af7ad70ee27bc032e8f7fb157eb7b0b0ffe0b2c7
Programador Cego <programadorcego@gmail.com>
2023-04-24 07:20:15 -0300

    Aula 25: Erros

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
-class Pessoa {
-	#nome;
-	
-	get nome(){
-		return this.#nome;
-	}
-	
-	set nome(valor){
-		this.#nome = valor;
-	}
+/*
+Ao executar um código JavaScript, diferentes erros podem ocorrer.
+Os erros podem ser erros de codificação cometidos pelo programador, erros devidos a entrada incorreta e outras coisas imprevisíveis.
+Para lidarmos com erros no JavaScript, podemos utilizar o bloco try/catch.
+A instrução try permite que você defina um bloco de código para testar erros enquanto está sendo executado.
+A instrução catch permite definir um bloco de código a ser executado, caso ocorra um erro no bloco try.
+As instruções JavaScript try e catch vêm em pares.
+*/
+
+function funcao(){
+	return "Olá, mundo!";
 }
 
-let pessoa = new Pessoa;
-pessoa.nome = "Willian";
-console.log(pessoa.nome);
+try {
+	if(typeof funcao === "undefined") throw "A função funcao não está definida!";
+	
+	console.log(funcao());
+} catch(err){
+	console.log(err);
+}
